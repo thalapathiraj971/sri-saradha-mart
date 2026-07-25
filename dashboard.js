da0997c7c951b8f74616d00673bfaa33cfecfcfa
thalapathiraj971 <thalapathiraj971@gmail.com>
2026-07-26 00:13:08 +0530

    Implement addProduct function for product listing
    Added a function to add products to the list with validation.

--- dashboard.js
+++ dashboard.js
@@ -1,1 +1,22 @@
+function addProduct(){
 
+const name=document.getElementById("productName").value;
+const price=document.getElementById("productPrice").value;
+
+if(name==="" || price===""){
+    alert("அனைத்து தகவல்களையும் நிரப்புங்கள்");
+    return;
+}
+
+const list=document.getElementById("list");
+
+list.innerHTML += `
+<div class="product">
+<h3>${name}</h3>
+<p>₹${price}</p>
+</div>
+`;
+
+document.getElementById("productName").value="";
+document.getElementById("productPrice").value="";
+}
