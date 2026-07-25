950bee218862541a946ae2bd82937dd30ad2664b
thalapathiraj971 <thalapathiraj971@gmail.com>
2026-07-26 00:10:55 +0530

    Add login function to handle user authentication

--- admin.js
+++ admin.js
@@ -1,1 +1,15 @@
+function login(){
 
+const pass=document.getElementById("password").value;
+
+if(pass==="12345"){
+
+window.location="dashboard.html";
+
+}else{
+
+alert("Wrong Password");
+
+}
+
+}
