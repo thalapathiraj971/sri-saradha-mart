9522cbe469fdd6917a3e21375cf49ddc5fb46512
thalapathiraj971 <thalapathiraj971@gmail.com>
2026-07-26 00:02:57 +0530

    Add search functionality to filter products

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+const search = document.getElementById("search");
 
+search.addEventListener("keyup", function () {
+
+    const value = search.value.toLowerCase();
+
+    const products = document.querySelectorAll(".product");
+
+    products.forEach((product) => {
+
+        const text = product.innerText.toLowerCase();
+
+        if(text.includes(value)){
+            product.style.display="block";
+        }else{
+            product.style.display="none";
+        }
+
+    });
+
+});
