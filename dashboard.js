ad6561f97455c9c79cdb631a11d40207b37be8e9
thalapathiraj971 <thalapathiraj971@gmail.com>
2026-07-27 23:22:36 +0530

    Integrate Firebase Firestore in addProduct function
    Refactor addProduct function to integrate Firebase Firestore for product addition.

--- dashboard.js
+++ dashboard.js
@@ -1,22 +1,43 @@
-function addProduct(){
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-app.js";
 
-const name=document.getElementById("productName").value;
-const price=document.getElementById("productPrice").value;
+import {
+  getFirestore,
+  collection,
+  addDoc
+} from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
 
-if(name==="" || price===""){
+const firebaseConfig = {
+  apiKey: "AIzaSyBQehMWwcThf8NLMGeJIG-omcywEEiJpHs",
+  authDomain: "raj-mini-mart.firebaseapp.com",
+  projectId: "raj-mini-mart",
+  storageBucket: "raj-mini-mart.firebasestorage.app",
+  messagingSenderId: "490305070206",
+  appId: "1:490305070206:web:ff8214149720a7b8a1e42f"
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+window.addProduct = async function () {
+
+  const name = document.getElementById("name").value;
+  const price = Number(document.getElementById("price").value);
+  const image = document.getElementById("image").value;
+
+  if (!name || !price || !image) {
     alert("அனைத்து தகவல்களையும் நிரப்புங்கள்");
     return;
-}
+  }
 
-const list=document.getElementById("list");
+  await addDoc(collection(db, "products"), {
+    name: name,
+    price: price,
+    image: image
+  });
 
-list.innerHTML += `
-<div class="product">
-<h3>${name}</h3>
-<p>₹${price}</p>
-</div>
-`;
+  alert("✅ Product Successfully Added");
 
-document.getElementById("productName").value="";
-document.getElementById("productPrice").value="";
-}
+  document.getElementById("name").value = "";
+  document.getElementById("price").value = "";
+  document.getElementById("image").value = "";
+};
