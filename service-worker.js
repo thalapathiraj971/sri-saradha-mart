7244100ffd66df3f40c76cc68243e480076cedec
ஸ்ரீ சாரதா மார்ட் <thalapathiraj971@gmail.com>
2026-07-29 13:21:06 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,62 @@
-const CACHE_NAME = "sri-saradha-mart-v1";
+const CACHE_NAME = "sri-saradha-mart-v2";
 
 const urlsToCache = [
-  "/",
-  "/index.html",
-  "/style.css",
-  "/script.js",
-  "/logo.png"
+    "/",
+    "/index.html",
+    "/style.css",
+    "/script.js",
+    "/logo.png"
 ];
 
+// Install Service Worker
 self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
-  );
+
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+
 });
 
+// Activate Service Worker
+self.addEventListener("activate", (event) => {
+
+    event.waitUntil(
+
+        caches.keys().then((cacheNames) => {
+
+            return Promise.all(
+
+                cacheNames.map((cache) => {
+
+                    if (cache !== CACHE_NAME) {
+                        return caches.delete(cache);
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+});
+
+// Fetch Files
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request)
-      .then((response) => response || fetch(event.request))
-  );
+
+    event.respondWith(
+
+        caches.match(event.request)
+            .then((response) => {
+
+                return response || fetch(event.request);
+
+            })
+
+    );
+
 });
