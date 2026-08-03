309ec7ede55f2f2269b498e14ce0e2cb626ca1c2
ஸ்ரீ சாரதா மார்ட் <thalapathiraj971@gmail.com>
2026-08-04 00:35:44 +0530

    Update WhatsApp order link in firebase.js

--- firebase.js
+++ firebase.js
@@ -31,7 +31,7 @@ async function loadProducts() {
         <img src="${p.image}" alt="${p.name}">
         <h3>${p.name}</h3>
         <p>₹${p.price}</p>
-        <a href="https://wa.me/916369135650" class="btn">Order on WhatsApp</a>
+        <a href="https://wa.me/918760534354" class="btn">Order on WhatsApp</a>
       </div>
     `;
   });
