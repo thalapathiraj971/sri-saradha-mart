4c457c3267dbc7a24bb539ee39d96c51ba5ca2e6
ஸ்ரீ சாரதா மார்ட் <thalapathiraj971@gmail.com>
2026-09-10 21:20:54 +0530

    Update service worker cache version and files

--- service-worker.js
+++ service-worker.js
@@ -1,62 +1,40 @@
-const CACHE_NAME = "sri-saradha-mart-v100";
+const CACHE_NAME = "sri-saradha-mart-v101";
 
 const urlsToCache = [
-    "/",
-    "/index.html",
-    "/style.css",
-    "/script.js",
-    "/logo.png"
+    "./",
+    "./index.html",
+    "./style.css",
+    "./script.js",
+    "./manifest.json",
+    "./logo.png"
 ];
 
-// Install Service Worker
 self.addEventListener("install", (event) => {
-
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then((cache) => {
-                return cache.addAll(urlsToCache);
-            })
+            .then((cache) => cache.addAll(urlsToCache))
     );
-
 });
 
-// Activate Service Worker
 self.addEventListener("activate", (event) => {
-
     event.waitUntil(
-
         caches.keys().then((cacheNames) => {
-
             return Promise.all(
-
                 cacheNames.map((cache) => {
-
                     if (cache !== CACHE_NAME) {
                         return caches.delete(cache);
                     }
-
                 })
-
             );
-
         })
-
     );
-
 });
 
-// Fetch Files
 self.addEventListener("fetch", (event) => {
-
     event.respondWith(
-
         caches.match(event.request)
             .then((response) => {
-
                 return response || fetch(event.request);
-
             })
-
     );
-
 });
